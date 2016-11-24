a16e53f45fc0ed6031e0b4d65ec0739b7963a4af
Sechenov Pavel <sechenov_pa@mail.ru>
2016-11-24 16:43:57 +0300

    flake8 settings

--- install/deploy_python.sh
+++ install/deploy_python.sh
@@ -1,3 +1,8 @@
+sudo pip install flake8
+
+mkdir -p ~/.config
+cp ~/.vim/.config/flake8 ~/.config
+
 ln -sf ~/.vim/vimrc.d/avail.d/common.vim ~/.vim/vimrc.d/conf.d/common.vim
 ln -sf ~/.vim/vimrc.d/avail.d/python.vim ~/.vim/vimrc.d/conf.d/python.vim
 
